333b36ea07a914c8b6e3884397be52c1ae4fc171
Nelly Kuznetsova <nelly@fwd.be>
2024-06-24 12:11:57 +0200

    Add interpreter

--- interpreter/interpreter.go
+++ interpreter/interpreter.go
@@ -1,1 +1,114 @@
 package interpreter
+
+import (
+	"github.com/nesyuk/golox/scanner"
+	"github.com/nesyuk/golox/token"
+)
+
+type Interpreter struct {
+}
+
+func (i *Interpreter) Eval(expr token.Expr) interface{} {
+	return expr.Accept(i)
+}
+
+func (i *Interpreter) VisitLiteral(expr *token.Literal) interface{} {
+	return expr.Value
+}
+
+func (i *Interpreter) VisitUnary(expr *token.Unary) interface{} {
+	right := i.Eval(expr.Right)
+	switch expr.Operation.TokenType {
+	case scanner.BANG:
+		return !i.isTruthy(right)
+	case scanner.MINUS:
+
+		return -1 * right.(float64)
+	}
+	//TODO: handle other cases error
+	return nil
+}
+
+func (i *Interpreter) VisitBinary(expr *token.Binary) interface{} {
+	left := i.Eval(expr.Left)
+	right := i.Eval(expr.Right)
+
+	switch expr.Operation.TokenType {
+	case scanner.MINUS:
+		return left.(float64) - right.(float64)
+	case scanner.SLASH:
+		return left.(float64) / right.(float64)
+	case scanner.STAR:
+		return left.(float64) * right.(float64)
+	case scanner.PLUS:
+		switch l := left.(type) {
+		case float64:
+			r, ok := right.(float64)
+			if ok {
+				return l + r
+			}
+			//TODO: handle error if !ok
+		case string:
+			r, ok := right.(string)
+			if ok {
+				return l + r
+			}
+			//TODO: handle error if !ok
+		}
+	case scanner.GREATER:
+		return left.(float64) > right.(float64)
+	case scanner.GREATER_EQUAL:
+		return left.(float64) >= right.(float64)
+	case scanner.LESS:
+		return left.(float64) < right.(float64)
+	case scanner.LESS_EQUAL:
+		return left.(float64) <= right.(float64)
+	case scanner.BANG_EQUAL:
+		return !i.isEqual(left, right)
+	case scanner.EQUAL_EQUAL:
+		return i.isEqual(left, right)
+	}
+	//TODO: handle error
+	return nil
+}
+
+func (i *Interpreter) VisitGrouping(expr *token.Grouping) interface{} {
+	return i.Eval(expr.Expression)
+}
+
+func (i *Interpreter) isTruthy(value interface{}) bool {
+	if value == nil {
+		return false
+	}
+	if val, isBool := value.(bool); isBool {
+		return val
+	}
+	return true
+}
+
+func (i *Interpreter) isEqual(left, right interface{}) bool {
+	switch v1 := left.(type) {
+	case nil:
+		if right == nil {
+			return true
+		}
+		return false
+	case bool:
+		if v2, ok := right.(bool); ok {
+			return v1 == v2
+		}
+		return false
+	case float64:
+		if v2, ok := right.(float64); ok {
+			return v1 == v2
+		}
+		return false
+	case string:
+		if v2, ok := right.(string); ok {
+			return v1 == v2
+		}
+		return false
+	}
+
+	return false
+}
--- interpreter/interpreter_test.go
+++ interpreter/interpreter_test.go
@@ -1,1 +1,202 @@
 package interpreter
+
+import (
+	"github.com/nesyuk/golox/scanner"
+	"github.com/nesyuk/golox/token"
+	"testing"
+)
+
+func TestInterpretLiteralFloat(t *testing.T) {
+	expr := &token.Literal{Value: 123.}
+	i := &Interpreter{}
+	got := i.Eval(expr)
+	result, ok := got.(float64)
+	if !ok {
+		t.Fatalf("expected float64 got %T", got)
+	}
+	if result != 123. {
+		t.Fatalf("expected 123. got %f", result)
+	}
+}
+
+func TestInterpretLiteralString(t *testing.T) {
+	expr := &token.Literal{Value: "abc"}
+	i := &Interpreter{}
+	got := i.Eval(expr)
+	result, ok := got.(string)
+	if !ok {
+		t.Fatalf("expected string got %T", got)
+	}
+	if result != "abc" {
+		t.Fatalf("expected abc got %v", result)
+	}
+}
+
+func TestInterpretLiteralBoolean(t *testing.T) {
+	expr := &token.Literal{Value: true}
+	i := &Interpreter{}
+	got := i.Eval(expr)
+	result, ok := got.(bool)
+	if !ok {
+		t.Fatalf("expected bool got %T", got)
+	}
+	if result != true {
+		t.Fatalf("expected true got %v", result)
+	}
+}
+
+func TestInterpretUnaryMinus(t *testing.T) {
+	expr := &token.Unary{
+		Operation: scanner.Token{TokenType: scanner.MINUS},
+		Right:     &token.Literal{Value: 123.0},
+	}
+	i := &Interpreter{}
+	got := i.Eval(expr)
+	if got != -123.0 {
+		t.Fatalf("expect: %v got: %v", -123.0, got)
+	}
+}
+
+func TestInterpretUnaryBang(t *testing.T) {
+	tests := []struct {
+		expr   token.Expr
+		expect bool
+	}{
+		{&token.Unary{
+			Operation: scanner.Token{TokenType: scanner.BANG},
+			Right:     &token.Literal{Value: 123.0},
+		}, false},
+		{&token.Unary{
+			Operation: scanner.Token{TokenType: scanner.BANG},
+			Right:     &token.Literal{Value: "abc"},
+		}, false},
+		{&token.Unary{
+			Operation: scanner.Token{TokenType: scanner.BANG},
+			Right:     &token.Literal{Value: true},
+		}, false},
+		{&token.Unary{
+			Operation: scanner.Token{TokenType: scanner.BANG},
+			Right:     &token.Literal{Value: false},
+		}, true},
+	}
+	i := &Interpreter{}
+	for _, test := range tests {
+		got := i.Eval(test.expr)
+		if got != test.expect {
+			t.Fatalf("expect: %v got: %v", test.expect, got)
+		}
+	}
+}
+
+func TestInterpretBinary(t *testing.T) {
+	tests := []struct {
+		expr   token.Expr
+		expect interface{}
+	}{
+		{&token.Binary{
+			Left:      &token.Literal{Value: 40.02},
+			Operation: scanner.Token{TokenType: scanner.PLUS},
+			Right:     &token.Literal{Value: 2.40},
+		}, 42.42},
+		{&token.Binary{
+			Left:      &token.Literal{Value: "Hello, "},
+			Operation: scanner.Token{TokenType: scanner.PLUS},
+			Right:     &token.Literal{Value: "World!"},
+		}, "Hello, World!"},
+		{&token.Binary{
+			Left:      &token.Literal{Value: 16.0},
+			Operation: scanner.Token{TokenType: scanner.STAR},
+			Right:     &token.Literal{Value: 2.0},
+		}, 32.0},
+		{&token.Binary{
+			Left:      &token.Literal{Value: 256.0},
+			Operation: scanner.Token{TokenType: scanner.SLASH},
+			Right:     &token.Literal{Value: 2.0},
+		}, 128.0},
+		{&token.Binary{
+			Left:      &token.Literal{Value: 2.0},
+			Operation: scanner.Token{TokenType: scanner.GREATER},
+			Right:     &token.Literal{Value: 1.0},
+		}, true},
+		{&token.Binary{
+			Left:      &token.Literal{Value: 2.4},
+			Operation: scanner.Token{TokenType: scanner.GREATER_EQUAL},
+			Right:     &token.Literal{Value: 2.4},
+		}, true},
+		{&token.Binary{
+			Left:      &token.Literal{Value: 2.0},
+			Operation: scanner.Token{TokenType: scanner.LESS},
+			Right:     &token.Literal{Value: 1.0},
+		}, false},
+		{&token.Binary{
+			Left:      &token.Literal{Value: 2.4},
+			Operation: scanner.Token{TokenType: scanner.LESS_EQUAL},
+			Right:     &token.Literal{Value: 2.4},
+		}, true},
+		{&token.Binary{
+			Left:      &token.Literal{Value: 2.4},
+			Operation: scanner.Token{TokenType: scanner.EQUAL_EQUAL},
+			Right:     &token.Literal{Value: 2.4},
+		}, true},
+		{&token.Binary{
+			Left:      &token.Literal{Value: "lox"},
+			Operation: scanner.Token{TokenType: scanner.EQUAL_EQUAL},
+			Right:     &token.Literal{Value: "lox"},
+		}, true},
+		{&token.Binary{
+			Left:      &token.Literal{Value: 2.4},
+			Operation: scanner.Token{TokenType: scanner.BANG_EQUAL},
+			Right:     &token.Literal{Value: 2.4},
+		}, false},
+		{&token.Binary{
+			Left:      &token.Literal{Value: "lox"},
+			Operation: scanner.Token{TokenType: scanner.BANG_EQUAL},
+			Right:     &token.Literal{Value: "lox"},
+		}, false},
+		{&token.Binary{
+			Left:      &token.Literal{Value: "lox"},
+			Operation: scanner.Token{TokenType: scanner.BANG_EQUAL},
+			Right:     &token.Literal{Value: nil},
+		}, true},
+		{&token.Binary{
+			Left:      &token.Literal{Value: 2.4},
+			Operation: scanner.Token{TokenType: scanner.BANG_EQUAL},
+			Right:     &token.Literal{Value: "lox"},
+		}, true},
+	}
+	i := &Interpreter{}
+	for _, test := range tests {
+		got := i.Eval(test.expr)
+		if got != test.expect {
+			t.Fatalf("expect: %v got: %v", test.expect, got)
+		}
+	}
+}
+
+func TestInterpretBinaryDivisionByZero(t *testing.T) {
+	t.Skip()
+	tests := []struct {
+		expr   token.Expr
+		expect interface{}
+	}{
+		{&token.Binary{
+			Left:      &token.Literal{Value: 256.0},
+			Operation: scanner.Token{TokenType: scanner.SLASH},
+			Right:     &token.Literal{Value: 0.0},
+		}, nil},
+	}
+	i := &Interpreter{}
+	for _, test := range tests {
+		got := i.Eval(test.expr)
+		if got != test.expect {
+			t.Fatalf("expect: %v got: %v", test.expect, got)
+		}
+	}
+}
+
+func TestIsTruthy(t *testing.T) {
+	i := &Interpreter{}
+	if i.isTruthy(nil) {
+		t.Fatalf("expected false")
+	}
+}
